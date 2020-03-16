2321a4f22d57d4a95112eaba025eb94adc668d2e
Jelle Swaanen <jelle.swaanen@gmail.com>
2020-03-16 16:22:54 +0100

    Cleaned up some commands

--- commands/render.js
+++ commands/render.js
@@ -1,7 +1,7 @@
-exports.run = (client, message, args, box) => {
+exports.run = (client, message, args) => {
   /*let renderPolygonCount = Math.floor(Math.abs(Math.random() * (20000-1000) + 1000));
   let renderTime = (5 * renderPolygonCount) ^ 3 + 2053;
   message.channel.send('> Rendering Done! It took ' + Math.floor(renderTime/3600) + 'h ' + Math.floor(renderTime % 60) + 'm ' + Math.floor(Math.random() * renderTime % 60) + 's to render ' + renderPolygonCount + ' polygons!');
-  console.log(`Render command used by ${message.author.username}`);
-  message.delete();*/
+  console.log(`Render command used by ${message.author.username}`);*/
+  message.delete();
 }

e6c385297c6561baae15b994f1faac4167717c66
Jelle Swaanen <jelle.swaanen@gmail.com>
2021-01-20 11:39:44 +0100

    Made reactions only work on TEST

--- messages/messageHandler.js
+++ messages/messageHandler.js
@@ -1,24 +1,30 @@
 exports.run = (client, message) => {
-  // console.log(message);
+  console.log(message);
   let messageLowerCase = message.content.toLowerCase();
-  
-  // Cussing Bot
-  for (word of client.box.swearWords) {
-    if (messageLowerCase.search(word) >= 0) {
-      console.log(`Found "${word}" in "${message.content}" sent by "${message.author.username}".`);
-      message.react('❌');
-      break;
+    
+  if (message.channel.guild.id == client.config.guilds.TEST.serverID) {
+    // Cussing Bot
+    for (word of client.box.swearWords) {
+      if (messageLowerCase.search(word) >= 0) {
+        console.log(`Found "${word}" in "${message.content}" sent by "${message.author.username}".`);
+        message.react('❌');
+        break;
+      }
     }
-  }
 
-  if (message.content == `<@234293993033170944>` || message.content == (`<@!234293993033170944>`)) {
-    message.channel.send(`"Hey stop that" - TheBlackCat 2018`);
-    console.log(`Responded with "Hey stop that" - TheBlackCat 2018 to "${message.author.username}".`);
-  }
-
-  if (message.content.search(client.config.ownerID) >= 0 || message.content.search(client.config.BotClientID) >= 0) { //NO U command
-    if (message.author.bot) return;
-    message.reply(' NO U!');
-    console.log(`Replied "NO U!" to "${message.author.username}".`);
+    // Replies to pings of blackcat
+    if (message.content == `<@234293993033170944>` || message.content == (`<@!234293993033170944>`)) {
+      message.channel.send(`"Hey stop that" - TheBlackCat 2018`);
+      console.log(`Responded with "Hey stop that" - TheBlackCat 2018 to "${message.author.username}".`);
+    }
+  
+    // If the bot or author are pinged send a NO U message
+    if (message.content.search(client.config.ownerID) >= 0 || message.content.search(client.config.BotClientID) >= 0) { //NO U command
+      if (message.author.bot) return;
+      message.reply(' NO U!');
+      console.log(`Replied "NO U!" to "${message.author.username}".`);
+    }
+  } else {
+    return;
   }
 }
